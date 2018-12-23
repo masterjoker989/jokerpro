b44010580cef6b7d4ec1614e64ccae273e2bda43
masterjoker989 <46088660+masterjoker989@users.noreply.github.com>
2018-12-23 23:50:16 +0530

    Update mjoker.py

--- mjoker.py
+++ mjoker.py
@@ -36,19 +36,6 @@ def is_marcos(ctx):
 
 
 
-@client.command(pass_context=True)
-async def tweet(ctx, usernamename:str, *, txt:str):
-    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get(url) as r:
-            res = await r.json()
-            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-            embed.set_image(url=res['message'])
-            embed.title = "{} twitted: {}".format(usernamename, txt)
-            await client.say(embed=embed)
-
-
 		
 	
 @client.command(pass_context = True)
@@ -77,59 +64,6 @@ async def ping(ctx):
     await client.say("Ping: {}ms".format(round((t2-t1)*1000)))
 
 
-
-
-@client.event
-async def on_member_join(member):
-    for channel in member.server.channels:
-        if channel.name == '★彡-welcome-彡★':
-            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
-            embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
-            embed.add_field(name='Your join position is', value=member.joined_at)
-            embed.set_thumbnail(url=member.avatar_url)
-            await client.send_message(channel, embed=embed)
-
-		
-
-@client.event
-async def on_member_join(member):
-    for channel in member.server.channels:
-        if channel.name == '🎉-welcome-🎊':
-            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
-            embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
-            embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/486489391083159574/520207004334292992/Loading.gif') 
-            embed.set_image(url = member.avatar_url)
-            embed.add_field(name='__Join position__', value='{}'.format(str(member.server.member_count)), inline=True)
-            embed.add_field(name='Time of joining', value=member.joined_at)
-            await client.send_message(channel, embed=embed)
-
-	
-@client.event
-async def on_member_remove(member):
-    for channel in member.server.channels:
-        if channel.name == '🎉-welcome-🎊':
-            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(title=f'{member.name} just left {member.server.name}', description='Bye bye 👋! We will miss you 😢', color = discord.Color((r << 16) + (g << 8) + b))
-            embed.add_field(name='__User left__', value='**Hope you will be back soon 😕.**', inline=True)
-            embed.add_field(name='Your join position was', value=member.joined_at)
-            embed.set_thumbnail(url=member.avatar_url)
-            await client.send_message(channel, embed=embed)
-
-
-@client.command(pass_context = True)
-@commands.has_permissions(administrator=True)
-async def setupwelcome(ctx):
-    if ctx.message.author.bot:
-      return
-    else:
-      server = ctx.message.server
-      everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
-      everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
-      await client.create_channel(server, '🎉-welcome-🎊',everyone)
-
-	
 
 
 
